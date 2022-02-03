711c400414d0ce53090c51b1847c2a727374683d
Zakharov <zakharov.products@gmail.com>
2022-02-03 17:40:07 +0300

    added_content_change

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,4 +1,5 @@
 const tabsElements = document.querySelectorAll('.tabs__button');
+const contentElements = document.querySelectorAll('.projects__content');
 
 function activeTabs() {
     tabsElements.forEach(element => {
@@ -7,8 +8,28 @@ function activeTabs() {
                 element.classList.remove('tabs__button_active');
             })
             event.target.classList.add('tabs__button_active');
+            activeContent(event.target.id);
         })
     })
 }
 
 activeTabs();
+
+function activeContent(idValueTab) {
+    if (idValueTab === 'udemy') {
+        contentElements.forEach(element => {
+            element.classList.remove('projects__content_active');
+        })
+        document.querySelector('[data-content="udemy"]').classList.add('projects__content_active');
+    } else if (idValueTab === 'geekbrains') {
+        contentElements.forEach(element => {
+            element.classList.remove('projects__content_active');
+        })
+        document.querySelector('[data-content="geekbrains"]').classList.add('projects__content_active');
+    } else if (idValueTab === 'test') {
+        contentElements.forEach(element => {
+            element.classList.remove('projects__content_active');
+        })
+        document.querySelector('[data-content="test"]').classList.add('projects__content_active');
+    }    
+}
