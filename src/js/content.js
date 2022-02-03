3d3fb6010b83e934edaa509df6866410058c3603
Zakharov <zakharov.products@gmail.com>
2022-02-03 19:24:34 +0300

    added_function_changingCardInformation

--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,7 @@
 const tabsElements = document.querySelectorAll('.tabs__button');
 const contentElements = document.querySelectorAll('.projects__content');
+const moreElements = document.querySelectorAll('.project__more');
+const backElements = document.querySelectorAll('.project__back');
 
 function activeTabs() {
     tabsElements.forEach(element => {
@@ -18,7 +20,7 @@ activeTabs();
 function activeContent(idValueTab) {
     contentElements.forEach(element => {
         element.classList.remove('projects__content_active');
-    })
+    });
     if (idValueTab === 'udemy') {        
         document.querySelector('[data-content="udemy"]').classList.add('projects__content_active');
     } else if (idValueTab === 'geekbrains') {
@@ -27,3 +29,20 @@ function activeContent(idValueTab) {
         document.querySelector('[data-content="test"]').classList.add('projects__content_active');
     }    
 }
+
+function changingCardInformation() {
+    moreElements.forEach(element => {
+        element.addEventListener('click', event => {
+            event.target.parentNode.classList.remove('project__generic_active');
+            event.target.parentNode.nextSibling.classList.add('project__details_active');
+        })
+    })
+    backElements.forEach(element => {
+        element.addEventListener('click', event => {
+            event.target.parentNode.classList.remove('project__details_active');
+            event.target.parentNode.previousSibling.classList.add('project__generic_active');
+        })
+    })
+}
+
+changingCardInformation();
