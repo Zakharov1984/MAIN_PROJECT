a1233be2469df7a50f28b60a00b979992cdff0a3
Zakharov <zakharov.products@gmail.com>
2022-02-27 21:09:30 +0300

    added_validation_form

--- src/js/form.js
+++ src/js/form.js
@@ -2,7 +2,9 @@ const formElement = document.querySelector('.contact__form');
 const nameFormElement = document.querySelector('.contact__form-input[id="name"]');
 const EmailFormElement = document.querySelector('.contact__form-input[id="email"]');
 const contentFormElement = document.querySelector('.contact__content[id="contentForm"]')
-const regExpNameForm = /^[a-zа-яёЁ]{1-15}$/i;
+const closeFormElement = document.querySelector('.form-error__close');
+const regExpNameForm = /^[a-zа-яёЁ]{1,15}$/i;
+const regExpEmailForm = /^[\w._-]+@\w+\.[a-z]{2,4}$/i;
 
 formElement.addEventListener('submit', event => {
     event.preventDefault();
@@ -16,6 +18,11 @@ nameFormElement.addEventListener('change', event => {
     }
 })
 
+EmailFormElement.addEventListener('change', event => {
+    if (!regExpEmailForm.test(event.target.value)) {
+        addedMarkupError(event.target);
+    }
+})
 
 
 
@@ -23,10 +30,20 @@ function addedMarkupError(item) {
     if (item.id == 'name') {
         if (!item.nextElementSibling.classList.contains('form-error')) {
             contentFormElement.insertAdjacentHTML('afterbegin', '<div class="form-error">Пожалуйста, введите корректное имя.<br>От 1 до 15 символов, допускаются буквы кириллицы и латинские символы.<div class="form-error__close"></div></div>');
+            const closeFormElement = document.querySelector('.form-error__close');
+            closeFormElement.addEventListener('click', event => {
+                document.querySelector('.form-error').remove();
+                item.value = '';
+            })
         }    
     } else if (item.id == 'email') {
         if (!item.nextElementSibling.classList.contains('form-error')) {
-            contentFormElement.insertAdjacentHTML('afterbegin', '<div class="form-error">Пожалуйста, введите корректный адрес почты.</div>');
+            contentFormElement.insertAdjacentHTML('afterbegin', '<div class="form-error">Пожалуйста, введите корректный адрес почты.<div class="form-error__close"></div></div>');
+            const closeFormElement = document.querySelector('.form-error__close');
+            closeFormElement.addEventListener('click', event => {
+                document.querySelector('.form-error').remove();
+                item.value = '';
+            })
         }    
     }
 }
